a784086934989bd67d22c6f0f3d7231f5296232a
Gilbert <android.wdmz.zc@gmail.com>
2015-08-24 20:38:01 +0800

    delete note

--- AndroidTest/app/src/main/java/zc/com/android/view/CustomView.java
+++ AndroidTest/app/src/main/java/zc/com/android/view/CustomView.java
@@ -22,125 +22,64 @@ import zc.com.androidtest.R;
 public class CustomView extends View {
     private static final String TAG = "CustomView";
 
-    /**
-     * 鑴ュ綍鑴濆崲Bitmap
-     */
     private Bitmap imageBitmap;
-    /**
-     * 鑴ュ綍鑴濆崲纰岃剾椴侀檱椹撮搯鍗よ劆
-     */
+    
+
     private float imageAspectRatio;
-    /**
-     * 鑴ュ綍鑴濆崲纰岃剾鑴ヨ祩鑴欐闇茶劆
-     */
+
     private float imageAlpha;
-    /**
-     * 鑴ュ綍鑴濆崲纰岃剾鑴宠锤padding
-     */
+
     private int imagePaddingLeft;
-    /**
-     * 鑴ュ綍鑴濆崲纰岃剾鑴¤劎padding
-     */
+
     private int imagePaddingTop;
-    /**
-     * 鑴ュ綍鑴濆崲纰岃剾鑴劒padding
-     */
+
     private int imagePaddingRight;
-    /**
-     * 鑴ュ綍鑴濆崲纰岃剾鑴ц剹padding
-     */
+
     private int imagePaddingBottom;
-    /**
-     * 鑴ュ綍鑴濆崲鑴＄煕鑴ｇ帿鑴涙嫝鑴㈤檰
-     */
+
     private int imageScaleType;
-    /**
-     * 鑴ュ綍鑴濆崲鑴＄煕鑴ｇ帿鑴涙嫝鑴㈤檰椴佹嫝鑴曢┐ fillXY
-     */
+
     private static final int SCALE_TYPE_FILLXY = 0;
-    /**
-     * 鑴ュ綍鑴濆崲鑴＄煕鑴ｇ帿鑴涙嫝鑴㈤檰椴佹嫝鑴曢┐ center
-     */
+
     private static final int SCALE_TYPE_CENTER = 1;
 
-    /**
-     * 鍗ら敋鑴よ姃鑴﹁剾鍗ゆ埉鑴涜劮鑴犺劵
-     */
     private String titleText;
-    /**
-     * 鍗ら敋鑴よ姃鑴﹁剾鍗ゆ埉鑴宠劙鑴ゆ皳楹撹锤鑴ㄩ殕
-     */
+
     private int titleTextSize;
-    /**
-     * 鍗ら敋鑴よ姃鑴﹁剾鍗ゆ埉鑴宠劙鑴ゆ皳鑴╄劗鑴¤姦
-     */
+
     private int titleTextColor;
-    /**
-     * 鍗ら敋鑴よ姃鑴﹁剾鍗ゆ埉鑴熼叾鑴矊鑴宠锤padding
-     */
+
     private int titlePaddingLeft;
-    /**
-     * 鍗ら敋鑴よ姃鑴﹁剾鍗ゆ埉鑴熼叾鑴矊鑴¤劎padding
-     */
+
     private int titlePaddingTop;
-    /**
-     * 鍗ら敋鑴よ姃鑴﹁剾鍗ゆ埉鑴熼叾鑴矊鑴劒padding
-     */
+
     private int titlePaddingRight;
-    /**
-     * 鍗ら敋鑴よ姃鑴﹁剾鍗ゆ埉鑴熼叾鑴矊鑴ц剹padding
-     */
+
     private int titlePaddingBottom;
 
-    /**
-     * 鑴宠劔鍗ら敋鑴よ姃鑴﹁剾鍗ゆ埉鑴涜劮鑴犺劵
-     */
     private String subTitleText;
-    /**
-     * 鑴宠劔鍗ら敋鑴よ姃鑴﹁剾鍗ゆ埉鑴宠劙鑴ゆ皳楹撹锤鑴ㄩ殕
-     */
+
     private int subTitleTextSize;
-    /**
-     * 鑴宠劔鍗ら敋鑴よ姃鑴﹁剾鍗ゆ埉鑴宠劙鑴ゆ皳鑴╄劗鑴¤姦
-     */
+
     private int subTitleTextColor;
-    /**
-     * 鑴宠劔鍗ら敋鑴よ姃鑴﹁剾鍗ゆ埉鑴熼叾鑴矊鑴宠锤padding
-     */
+
     private int subTitlePaddingLeft;
-    /**
-     * 鑴宠劔鍗ら敋鑴よ姃鑴﹁剾鍗ゆ埉鑴熼叾鑴矊鑴¤劎padding
-     */
+
     private int subTitlePaddingTop;
-    /**
-     * 鑴宠劔鍗ら敋鑴よ姃鑴﹁剾鍗ゆ埉鑴熼叾鑴矊鑴劒padding
-     */
+
     private int subTitlePaddingRight;
-    /**
-     * 鑴宠劔鍗ら敋鑴よ姃鑴﹁剾鍗ゆ埉鑴熼叾鑴矊鑴ц剹padding
-     */
+
     private int subTitlePaddingBottom;
 
-    /**
-     * 椹磋劥褰曢晛鑴剻纰岃剾paint
-     */
     private Paint paint;
     private TextPaint textPaint;
-    /**
-     * 鑴剻鑴岄簱闄嗚幗闇茬瘬椹磋劥褰曢晛鑴拌劏铏忕鑴ュ崲铏忛┐璺劙纰岃剾绂勫繖鑴拌劃鑴熼叾鑴矊
-     */
+
     private Rect rect;
-    /**
-     * 椹撮搯闇茶劆娼炶劌璧傝劷闇茶劆纰岃剾鑴冲嵂鑴ㄩ殕鑴扮
-     */
+
     private static final int MIN_SIZE = 12;
-    /**
-     * 椹磋劥褰曢晛纰岃剾椹撮搯闇茶劆
-     */
+
     private int mViewWidth;
-    /**
-     * 椹磋劥褰曢晛纰岃剾璧傝劷闇茶劆
-     */
+
     private int mViewHeight;
 
     public CustomView(Context context, AttributeSet attrs, int defStyle) {
@@ -156,10 +95,10 @@ public class CustomView extends View {
                             getResources(), a.getResourceId(attr, 0));
                     break;
                 case R.styleable.CustomView_imageAspectRatio:
-                    imageAspectRatio = a.getFloat(attr, 1.0f);//鑴涘崲鑴犺劎椴侀檱椹撮搯鑴ц効纰岃劆
+                    imageAspectRatio = a.getFloat(attr, 1.0f);
                     break;
                 case R.styleable.CustomView_imageAlpha:
-                    imageAlpha = a.getFloat(attr, 1.0f);//鑴涘崲鑴犺劎铏忕鑴ヨ祩鑴欐
+                    imageAlpha = a.getFloat(attr, 1.0f);
                     if (imageAlpha > 1.0f) imageAlpha = 1.0f;
                     if (imageAlpha < 0.0f) imageAlpha = 0.0f;
                     break;
@@ -184,10 +123,10 @@ public class CustomView extends View {
                 case R.styleable.CustomView_titleTextSize:
                     titleTextSize = a.getDimensionPixelSize(
                             attr, (int) TypedValue.applyDimension(
-                                    TypedValue.COMPLEX_UNIT_SP, 25, getResources().getDisplayMetrics()));//鑴涘崲鑴犺劎鍗ら敋鑴よ姃鑴宠劙鑴ゆ皳楹撹锤鑴ㄩ殕25sp
+                                    TypedValue.COMPLEX_UNIT_SP, 25, getResources().getDisplayMetrics()));
                     break;
                 case R.styleable.CustomView_titleTextColor:
-                    titleTextColor = a.getColor(attr, 0x00000000);//鑴涘崲鑴犺劎娼炶劮鑴¤姦鑴宠劙鑴ゆ皳
+                    titleTextColor = a.getColor(attr, 0x00000000);
                     break;
                 case R.styleable.CustomView_titlePaddingLeft:
                     titlePaddingLeft = a.getDimensionPixelSize(attr, 0);
@@ -207,7 +146,7 @@ public class CustomView extends View {
                 case R.styleable.CustomView_subTitleTextSize:
                     subTitleTextSize = a.getDimensionPixelSize(attr,
                             (int) TypedValue.applyDimension(
-                                    20, TypedValue.COMPLEX_UNIT_SP, getResources().getDisplayMetrics()));//鑴涘崲鑴犺劎鑴宠劔鍗ら敋鑴よ姃鑴宠劙鑴ゆ皳楹撹锤鑴ㄩ殕20sp
+                                    20, TypedValue.COMPLEX_UNIT_SP, getResources().getDisplayMetrics()));
                     break;
                 case R.styleable.CustomView_subTitleTextColor:
                     subTitleTextColor = a.getColor(attr, 0x00000000);
